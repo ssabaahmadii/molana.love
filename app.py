57bb495c45895a1eaef27e3afe4c98684cb0aeed
ssabaahmadii <ssabaahmadii@gmail.com>
2025-03-25 21:20:10 +0330

    Fix port issue

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def contact():
 def molana():
     return render_template('molana.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if name == "__main__":
+    app.run(host="0.0.0.0", port=10000)
 
 from flask import Flask, render_template
 from models import db, Article
